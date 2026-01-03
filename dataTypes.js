9255ac7c1cad0c0fc5f193969732a997e643a2d9
Hammad Azam <mhammadazam1@gmail.com>
2026-01-03 23:39:44 +0500

    commit

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Welcome to Data Types in JavaScript"); // undefined dataType
+
+// ecmastrict is a documentaion for js check on tc39.es, otherwise use mdn docs
+
+// Data Types in JavaScript
+// 1. Primitive Data Types
+// number(2 to power 53), null, string, symbol(uniqueness), boolean, bigint, undefined  (nnssbbu)
+
+// 2. Non-Primitive Data Types
+// objects (arrays, functions, dates)
+
+console.log(typeof "hammad"); // string data type
+console.log(typeof 34); // number data type
+console.log(typeof 34.5); // number data type
+console.log(typeof true); // boolean data type
+console.log(typeof undefined); // undefined data type
+console.log(typeof null); // object data type (bug in js)
+console.log(typeof 9007199254741991n); // bigint data type
+console.log(typeof Symbol("hammad")); // symbol data type
+
+
+
--- let,const,var.js
+++ let,const,var.js
@@ -1,2 +1,20 @@
 
-console.log(hello);
+console.log("hello");
+
+const accountId = 112233; // can't be changed again
+let accountEmail = "hammad@gmail.com"; // can be changed, we will always use let
+var accountPassword = "hammad123";// can be changed, avoid using var bcz of issue in function scope and block scope
+accountCity = "Karachi"; // can be used like this but avoid it
+let accountState; // undefined
+
+
+// accountId = 334455; // error
+accountEmail = "hammad2@gmail.com";
+accountPassword = "hammad456";
+accountCity = "Lahore";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
