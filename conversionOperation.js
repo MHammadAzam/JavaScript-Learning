ad95b8a2e2304d4f680b7b9b66165031efc70c88
Hammad Azam <mhammadazam1@gmail.com>
2026-01-04 23:36:02 +0500

    commit

--- conversionOperation.js
+++ conversionOperation.js
@@ -8,17 +8,17 @@
 // string("hammad") => NaN
 
 let score = "100";
-console.log(typeof score); // string
+// console.log(typeof score); // string
 
 let valueInNumber = Number(score); // conversion from string to number
-console.log(typeof valueInNumber); // number
-console.log(valueInNumber); // 100
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 100
 
 // now what if someone has passed a score = "100abc", then it will also be converted to number but actually its NaN.
 let score2 = "100abc";
 let valueInNumber2 = Number(score2);
-console.log(typeof valueInNumber2); // number
-console.log(valueInNumber2); // NaN
+// console.log(typeof valueInNumber2); // number
+// console.log(valueInNumber2); // NaN
 
 // now if you have undefined and null values, null will be converted to 0 but undefined will be converted to NaN, same if you have boolean values, true will be converted to 1 and false to 0, also if you have string value e.g("hammad") then if will be converted to NaN.
 
@@ -32,10 +32,40 @@ console.log(valueInNumber2); // NaN
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // converting everything using String.
 let number = 33
 let stringNumber = String(number);
-console.log(stringNumber)
+// console.log(stringNumber)
+
+
+// ********************************* OPERATIONS *********************************
+
+let str1 = "hello"
+let str2 = " hammad"
+let str3 = str1 + str2; // concatenation
+console.log(str3)
+
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); // 12
+// console.log("1" + "2"); // 12
+// console.log(1 + 2 + "2"); // 32
+// console.log("1" + 2 + 2) // 122
+
+// PREFIX AND POSTFIX INCREMENTS
+// so in prefix the value is incremented first and in postfix the value is assigned first then incremented
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
