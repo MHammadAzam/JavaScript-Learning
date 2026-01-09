79e4e23b1eccd8ec1e0a3f9acd25601ae0adacd3
Hammad Azam <mhammadazam1@gmail.com>
2026-01-09 23:58:05 +0500

    commit

--- Basics2/11_Array.js
+++ Basics2/11_Array.js
@@ -39,3 +39,20 @@ console.log(numbers); // [9,5,2,1]
 myArr.reverse();
 console.log(myArr); // [5,4,3,0]
 
+
+const arr1 = [1,2];
+const arr2 = [3,4];
+const combined = arr1.concat(arr2);
+console.log(combined); // [1,2,3,4]
+
+
+const copyArr = [...myArr]; // copy array
+const combinedArr = [...arr1, ...arr2]; // combine arrays
+console.log(copyArr);
+console.log(combinedArr);
+
+
+console.log(myArr.some(num => num > 3));  // true
+console.log(myArr.every(num => num > 0)); // false
+
+
