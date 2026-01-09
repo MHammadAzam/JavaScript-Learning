7a2754f1e60df1a3b4f1320646a50540efa8caf0
Hammad Azam <mhammadazam1@gmail.com>
2026-01-09 23:56:31 +0500

    commit

--- Basics2/11_Array.js
+++ Basics2/11_Array.js
@@ -19,3 +19,23 @@ console.log(filteredArr);  // [4,5]
 
 const sum = myArr.reduce((accumulator, current) => accumulator + current, 0);
 console.log(sum);  // 0+3+4+5 = 12
+
+
+const found = myArr.find(num => num > 3);
+console.log(found);  // 4
+
+
+const foundIndex = myArr.findIndex(num => num > 3);
+console.log(foundIndex);  // 2
+
+const numbers = [5,2,9,1];
+numbers.sort();  // Default: sorts as strings
+console.log(numbers);  // [1,2,5,9]
+
+numbers.sort((a,b) => b-a); // Descending order
+console.log(numbers); // [9,5,2,1]
+
+
+myArr.reverse();
+console.log(myArr); // [5,4,3,0]
+
