b7169f88b63479e20721fcf33cd1025891b44e4b
Hammad Azam <mhammadazam1@gmail.com>
2026-01-09 23:55:27 +0500

    commit

--- Basics2/11_Array.js
+++ Basics2/11_Array.js
@@ -6,3 +6,16 @@ myArr.forEach((item, index) => {
 // 1 3
 // 2 4
 // 3 5
+
+
+const newArr = myArr.map(num => num * 2);
+console.log(newArr);  // [0,6,8,10]
+console.log(myArr);   // [0,3,4,5] original array unchanged
+
+
+const filteredArr = myArr.filter(num => num > 3);
+console.log(filteredArr);  // [4,5]
+
+
+const sum = myArr.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum);  // 0+3+4+5 = 12
