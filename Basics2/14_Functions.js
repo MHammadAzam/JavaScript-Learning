ff463382d86d5b5dce142588fcf4fb8a42170e36
Hammad Azam <mhammadazam1@gmail.com>
2026-01-14 00:26:40 +0500

    commit

--- Basics2/14_Functions.js
+++ Basics2/14_Functions.js
@@ -26,10 +26,42 @@ const result = multiply(5,4)
 console.log(result); // 20
 
 
-// 
+// 4.... concept about undefined when no argument is passed
 function loginUserMessage(username){
       return `${username} just logged in`
 }
 console.log(loginUserMessage()) // if you dont pass any argument it will print "undefined" just logged in.
 const message = loginUserMessage("john_doe")
 console.log(message); // john_doe just logged in
+
+
+// 5.... REST OPERATOR, e.g if you have multiple parameters and you dont know how many arguments will be passed
+function calculateCartPrice(...prices){
+       return prices
+}
+console.log(calculateCartPrice(20,30,40,50)); // [ 20, 30, 40, 50 ]
+
+// 6 ... passing object into function
+const user = {
+  username: "hammad",
+  price: 2000
+}
+
+function handleObject(anyObject){
+  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+handleObject(user); // Username is hammad and price is 2000
+handleObject({  // also you can directly pass object while calling a function
+  username: "alice",
+  price: 3500
+})
+
+
+// 7... passing array into function
+const myNewArray = [ 200, 400, 600, 800 ]
+
+function returnSecondValue(arr){
+       return arr[1]
+}
+console.log(returnSecondValue(myNewArray)); // 400
+console.log(returnSecondValue([ 5,10,15,20 ])); // 10 , directly passing array while calling function
