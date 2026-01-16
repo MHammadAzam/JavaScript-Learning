9894c77950811afcad7253f97f12eb7172593c31
Hammad Azam <mhammadazam1@gmail.com>
2026-01-16 23:18:25 +0500

    commit

--- Basics2/10_Arrays.js
+++ Basics2/10_Arrays.js
@@ -2,7 +2,7 @@
 // We can also create any array having mixed datatypes
 
 const myArr = [0,1,2,3,4,5]; // array literal syntax
-const myHeroes =["Shaktiman", "Doraemon", "Batman"];
+const myHeroes =["Shaktiman", "Doraemon", "Batman", "Superman" , "Spiderman"];
 
 const myArr2 = new Array(1, 2, 3, 4, 5); // array constructor syntax
 console.log(myArr[0]) // prints 0
@@ -33,3 +33,43 @@ console.log(SliceArr);
 
 const SpliceArr = myArr.splice(1,2); // removes 2 elements from index 1, changes original array
 console.log(SpliceArr); // prints [1,2]
+
+
+
+
+
+
+
+
+
+
+
+// PRACTICE QUESTIONS ON ARRAYS
+// 1
+// const fruits = ["Apple", "Banana", "Mango", "Orange"] // literal syntax
+// const animeCharacters = new Array("Luffy", "Naruto", "Goku", "Ichigo") // constructor syntax
+
+// // 2
+// fruits.push("pineapple") // adds pineapple at the end of fruits array
+// fruits.pop() // pops out the last element from fruits array
+
+// // 3
+// fruits.unshift("strawberry") // adds element at the start of an array
+// fruits.shift() // removes first element from an array
+
+// // 4
+// console.log(fruits.indexOf("Mango")) // if you give incorrect value it will return -1
+// console.log(fruits.includes('Apple')) // will return true
+
+// // 5
+// const slicing1 = fruits.slice(0,2) // it will slice just first 2 values and doesnt change original array
+// console.log(slicing1)
+// console.log(fruits)
+
+// const splicing1 = fruits.splice(0,2) // changes the original array 
+// console.log(splicing1)
+// console.log(fruits)
+
+// // 6
+// console.log(fruits.join(",")) // converts it into string and joins the elements with a comma
+
