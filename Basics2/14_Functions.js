e9898fcfa1006a0e0449948508fff4e05231d3b3
Hammad Azam <mhammadazam1@gmail.com>
2026-01-16 22:55:39 +0500

    commit

--- Basics2/14_Functions.js
+++ Basics2/14_Functions.js
@@ -65,3 +65,69 @@ function returnSecondValue(arr){
 }
 console.log(returnSecondValue(myNewArray)); // 400
 console.log(returnSecondValue([ 5,10,15,20 ])); // 10 , directly passing array while calling function
+
+
+
+
+
+
+
+// PERSONAL PRACTICE QUESTIONS ON FUNCTIONS
+// 1
+// function hammad(){
+//   console.log(hammad)
+// }
+// hammad()
+
+// // 2
+// function divideByTwo(num){
+//   console.log(num/2)
+// }
+// divideByTwo(50)
+
+// // 3
+// function greetUser(username){
+//   return username
+// }
+// const greeting = greetUser("hammad")
+// console.log(greeting)
+
+// // 4 undefined logic inside function
+// function userAge(age){
+//   return `your age is ${age} and its very good age for learning`
+
+// }
+// const ageMessage = userAge()
+// console.log(ageMessage) // your age is undefined and its very good age for learning
+
+// // 5 Rest Operator
+// function getColors(...colors){
+//   return colors
+// }
+// console.log(getColors("red", "blue", "green", "yellow")) // [ 'red', 'blue', 'green', 'yellow' ]  
+
+// // 6 passing object into function
+// const StdDetails = {
+//   name: "hammad",
+//   age: "22",
+//   degree: "Computer Science",
+//   Cgpa: 3.6
+// }
+
+// function handleObject(Myobject){
+//   return `Student name is ${Myobject.name}, age is ${Myobject.age}, and my degree is ${Myobject.degree}`
+// }
+// console.log(handleObject(StdDetails))
+// //     OR
+// console.log(handleObject({
+//   // write details here of another student
+// }))
+
+// // 7 passing an Array inside the function
+// const Arr3 =[100, 200, 300, 400, 500, 600, 700]
+// function returnForthValue(arr){
+//   return arr[3] + 1
+// }
+// console.log(returnForthValue(Arr3)) // 401
+// //      OR
+// console.log(returnForthValue([10,20,30,40,50])) // can pass values directly here
