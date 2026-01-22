da786a551adc418259b72ef33375ade07a1dae2e
Hammad Azam <mhammadazam1@gmail.com>
2026-01-23 00:08:53 +0500

    commit

---
+++ Basics3/21_WhileLoop.js
@@ -0,0 +1,23 @@
+// WHILE LOOP
+
+let i = 0
+while (i <= 10) {
+     console.log(i)
+     i++
+}
+
+let myArray = ['flash', 'batman', 'superman']
+let j = 0
+while(j < myArray.length){
+    console.log(myArray[j])
+    j++
+}
+
+
+// DO WHILE LOOP
+// it runs code one time must then check the condition
+let k = 1
+do {
+   console.log(k)
+   k++
+} while (k <= 10);
