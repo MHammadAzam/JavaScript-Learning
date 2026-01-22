7c28855c1e1d98a65e33db341f7d82a439932b7c
Hammad Azam <mhammadazam1@gmail.com>
2026-01-22 23:51:37 +0500

    commit

---
+++ Basics3/20_Loops.js
@@ -0,0 +1,35 @@
+// FOR LOOP
+// To change value of a varibale which is used multiple times in a code, double click on that variable which will select that variable then press ctrl + d
+
+//  declare variable, Condition check, itereate it
+// 1 ... SIMPLE FOR LOOP
+for (let i = 0; i < 10; i++) {
+  const element = i;
+
+  if(element == 5){
+        // console.log("5 is a perfect number")
+  }
+  // console.log(element)
+  
+}
+
+
+// 2: LOOP WITHIN A LOOP
+for(let i = 1; i<=10; i++){
+  // console.log("Outer Loop")
+    for(let j = 1; j<=10; j++){
+          // console.log("Inner Loop")
+          // console.log(i + "*" + j + "=" + (i*j))  it will print tables from 1 to 10
+    }
+}
+
+
+// 3: LOOP ON ARRAYS
+let myArray = ["flash", "batman", "superman", "King", "Bot", "Naruto"] // 6
+
+for(let i = 0 ; i<=myArray.length; i++){
+    console.log(myArray[i])
+}
+
+
+// 
